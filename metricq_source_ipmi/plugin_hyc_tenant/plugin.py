67b3f43c4b324657198a89ac3cc9f06dc74037fd
Julius Metz <julius.metz@tu-dresden.de>
2019-08-27 12:32:22 +0200

    plugin plugin_hyc_tenant_aperture modified

---
+++ metricq_source_ipmi/plugin_hyc_tenant/plugin.py
@@ -0,0 +1,20 @@
+NEEDED_SENSORS = 4
+TENANT_STATUS_ENABLED = "'Device Enabled'"
+
+def create_metric_value(sensors):
+    if {'HYB_1 Tenant', 'HYB_2 Tenant'}.issubset(sensors.keys()) and len(sensors) == NEEDED_SENSORS:
+        HYB_1 = float('nan')
+        HYB_2 = float('nan')
+        for sensor, sensor_data in sensors.items():
+            if sensor in ['HYB_1 Tenant', 'HYB_2 Tenant']:
+                continue
+            if sensor.startswith('HYB_1'):
+                HYB_1 = sensor_data['value']
+            elif sensor.startswith('HYB_2'):
+                HYB_2 = sensor_data['value']
+
+        if sensors['HYB_1 Tenant']['status'] == TENANT_STATUS_ENABLED:
+            return HYB_1
+        elif sensors['HYB_2 Tenant']['status'] == TENANT_STATUS_ENABLED:
+            return HYB_2
+    return float('nan')
--- metricq_source_ipmi/plugin_hyc_tenant_aperture/plugin_hyc_tenant_aperture.py
+++
@@ -1,9 +0,0 @@
-NEEDED_SENSORS = 4
-
-def create_metric_value(sensors):
-    if {'HYB_1 Pri Aper.', 'HYB_2 Pri Aper.', 'HYB_1 Tenant', 'HYB_2 Tenant'}.issubset(sensors.keys()):
-        if sensors['HYB_1 Tenant']['status'] == "'Device Enabled'":
-            return sensors['HYB_1 Pri Aper.']['value']
-        elif sensors['HYB_2 Tenant']['status'] == "'Device Enabled'":
-            return sensors['HYB_2 Pri Aper.']['value']
-    return float('nan')
